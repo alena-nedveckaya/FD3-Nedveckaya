3a027e16899cbb17e39a06f0295d4480df3c0dcb
alena-nedveckaya <alenashcherb@gmail.com>
2018-03-03 02:09:20 +0300

    selection item, add new buttons

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -11,16 +11,22 @@ var itemsTable = React.createClass({
             })
         )
     },
+    getInitialState: function(){
+        return {selectedItem:null}
+    },
+    isSelected:function(code){
+        this.setState({selectedItem:code})
+    },
 
     render: function(){
         
         var itemInformation = this.props.items.map ((v) =>
-           React.createElement (item, {itemInfo: v})
+           React.createElement (item, {itemInfo: v, cbIsSelected:this.isSelected, selectedItem:this.state.selectedItem})
         );
     
         return   React.DOM.div({className:"wrapper"},
                 React.DOM.h3({className:"title"}, this.props.title),
-                React.DOM.table({className:"itemsTable"}, itemInformation)
+                React.DOM.div({className:"itemsTable"}, itemInformation)
             )
             
 
@@ -31,14 +37,35 @@ var itemsTable = React.createClass({
 var item = React.createClass({
     displayName: 'item',
 
+    propTypes:{
+        itemInfo:React.PropTypes.object
+    },
+
+    isSelectedItem: function(EO){
+        this.props.cbIsSelected(this.props.itemInfo.code)
+    },
+
     render: function (){
-        React.DOM.tr({key:v.code, className:'itemTr'},
-                React.DOM.td({className:'itemTd'}, v.name),
-                React.DOM.td({className:'itemTd'}, v.price),
-                React.DOM.td({className:'itemTd'},
-                    React.DOM.img({className:'itemImg', src:v.url},)
-                ),
-                React.DOM.td({className:'itemTd'}, v.left)        
+
+        return  React.DOM.div({key:this.props.itemInfo.code, className:(this.props.itemInfo.code == this.props.selectedItem) ? 'itemTr selected' : 'itemTr'},
+            React.DOM.label(null,
+                    React.DOM.input({className:'hiddenRadio', 
+                                    code:this.props.itemInfo.code, 
+                                    type:'radio', name:'item', 
+                                    value:this.props.itemInfo.name,
+                                    onClick:this.isSelectedItem}),
+                    
+                        React.DOM.div({className:'itemTd'}, this.props.itemInfo.name),
+                        React.DOM.div({className:'itemTd'}, this.props.itemInfo.price),
+                        // React.DOM.div({className:'itemTd'},
+                        //     React.DOM.img({className:'itemImg', src:this.props.itemInfo.url},)
+                        // ),
+                        React.DOM.div({className:'itemTd'}, this.props.itemInfo.left),
+                        React.DOM.div({className:'itemTd'},
+                            React.DOM.button({className:'btn_edit' },'Редактировать')),
+                        React.DOM.div({className:'itemTd'},
+                            React.DOM.button({className:'btn_delete' },'Удалить')),
+                    )       
             )
     }
 })
