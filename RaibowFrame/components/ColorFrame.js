6d698dd93a99686c97fc9ba349e98c0dd74e0b3c
alena-nedveckaya <alenashcherb@gmail.com>
2018-03-11 23:30:08 +0300

    Add project RaibowFrame

---
+++ RaibowFrame/components/ColorFrame.js
@@ -0,0 +1,37 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class ColorFrame extends React.Component {
+
+  static propTypes = {
+    color: PropTypes.string.isRequired,
+    colorArr: PropTypes.array.isRequired,
+  };
+
+  state ={
+    colorArr:this.props.colorArr, lengthArr:this.props.colorArr.length
+  }
+  addBorder = ( colorArr, children, first) =>{
+    if (!first)
+      colorArr.splice(0,1)
+      return <div style={{border:"1px solid " +this.props.colorArr[0], padding:"5px"}}>
+            {
+              (colorArr.length >0)
+              ?
+                this.addBorder(colorArr,children, false)
+              :
+                children
+            }
+          </div>
+  }
+  render() {
+    return (
+      <div>
+        {this.addBorder(this.state.colorArr, this.props.children, true)}
+      </div>
+    );
+  }
+
+}
+
+export default ColorFrame;
