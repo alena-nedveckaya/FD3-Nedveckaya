9e4b91e0d1a6119fc2de2f76f07dca6a4a9512f0
alena-nedveckaya <alenashcherb@gmail.com>
2018-03-14 17:53:07 +0300

    edit EditForm.js (correct value in input tag)

--- Mobile50_immutable/components/EditForm.js
+++ Mobile50_immutable/components/EditForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-class EditForm extends React.Component{
+class EditForm extends React.PureComponent{
     static propTypes = {
         client:PropTypes.shape({
           id: PropTypes.number.isRequired,
@@ -11,25 +11,39 @@ class EditForm extends React.Component{
       };
 
       state = {
-        client: this.props.client,
+        client: this.props.client, newName:this.props.client.fio
       };
 
+    componentWillReceiveProps = (newProps) => {
+        console.log("EditForm componentWillReceiveProps");
+       this.setState({client: newProps.client, newName:newProps.client.fio})
+
+    };
+      changeFIO = (EO) =>{
+
+          this.setState({newName:EO.target.value});
+          console.log(this.state.newName)
+      };
+
+
     render(){
-        console.log('EditForm render')
-        console.log(this.props.client.fio)
+        console.log('EditForm render');
+        console.log(this.state.newName, this.state.client, this.props.client);
         return  <div>
                     <div>
                         <label>
                             ФИО
-                            <input defaultValue={this.props.client.fio}/>
+
+                            <input value={this.state.newName} onChange={this.changeFIO}/>
                         </label>
                     </div>
-                    <div>
-                        <label>
-                            Баланс
-                            <input defaultValue={this.props.client.balance}/>
-                        </label>
-                    </div>
+
+                    {/*<div>*/}
+                        {/*<label>*/}
+                            {/*Баланс1*/}
+                            {/*<input value={this.props.client.balance }/>*/}
+                        {/*</label>*/}
+                    {/*</div>*/}
 
                 </div>
     }
